3bf12fd564fe4321cd03c768254f5f4a7110ee47
vasik96 <leman.vaclav@gmail.com>
2024-03-16 23:11:37 +0100

    Initial release

---
+++ src/main/java/org/example/flymod/flymod/Fly_mod.java
@@ -0,0 +1,13 @@
+package org.example.flymod.flymod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Fly_mod implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
